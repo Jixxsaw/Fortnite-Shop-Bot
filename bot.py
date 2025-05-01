cca0cef81a1b3678e16c183f0b5453b38bcfe4f5
Jixxsaw <luckyluke.6601@gmail.com>
2025-05-01 10:45:07 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -72,7 +72,7 @@ async def fetch_shop_data():
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
         'Accept-Language': 'en-US,en;q=0.9',
-        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Encoding': 'gzip, deflate',  # Gzip und Deflate statt Brotli
         'Connection': 'keep-alive'
     }
 
